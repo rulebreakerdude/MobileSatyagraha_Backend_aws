6c3ab71a66f7d329f94034c584d1156bbe2a8d20
rulebreakerdude <rulebreakerdude@gmail.com>
2019-02-14 17:10:40 +0530

    modified mimetype

--- application.py
+++ application.py
@@ -40,7 +40,7 @@ def learn2earnRecordNumber(phoneNumber):
 	if request.method == 'GET':
 		z='{:%Y%m%d%H%M%S}'.format(datetime.datetime.now())
 		mydb.insertLearn2EarnRecordNumberData(phoneNumber,z)
-	return '1'
+	return current_app.response_class('1', mimetype="application/x-www-form-urlencoded")
 #****************************************************************************
 
 
