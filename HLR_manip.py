cb64861f34e9a79f83324a488303ebf79ae864c0
rulebreakerdude <rulebreakerdude@gmail.com>
2019-04-29 13:09:01 +0530

    cellone to bsnl map

--- HLR_manip.py
+++ HLR_manip.py
@@ -1,12 +1,12 @@
 import json
 import requests
 import datetime
-'''
+
 dict_HLR_op_code={}
 dict_HLR_op_code_new={}
 with open('HLR.json') as f:
 	dict_HLR_op_code = json.load(f)
-HLR_to_op={'aircel':'1','bsnl':'3','airtel':'28','vodafone':'22','docomo':'17','reliance':'13','idea':'8','uninor':'19','videocon':'5'}
+HLR_to_op={'aircel':'1','bsnl':'3', 'cellone':'3', 'airtel':'28','vodafone':'22','docomo':'17','reliance':'13','idea':'8','uninor':'19','videocon':'5'}
 op_code_map={'1':'AL','3':'BS','28':'AT','8':'IDX','10':'MS','12':'RL','13':'RG','17':'TD','19':'UN','5':'VD','22':'VF'}
 for mccmnc in dict_HLR_op_code:
 	lookup_query=dict_HLR_op_code[mccmnc]
@@ -19,3 +19,4 @@ print dict_HLR_op_code_new
 z='{:%Y%m%d%H%M%S}'.format(datetime.datetime.now())
 rech=requests.post("http://www.login.imwallet.in/API/APIService.aspx?userid=6264241440&pass=819954&mob=9630095766&opt=B&amt=10&agentid=%s&fmt=JSON" %(z))
 print json.loads(rech.text)['MSG']
+'''
--- application.py
+++ application.py
@@ -83,7 +83,7 @@ def HLR(tid,number):
 		dict_HLR_op_code_new={}
 		with open('HLR.json') as f:
 			dict_HLR_op_code = json.load(f)
-		HLR_to_op={'aircel':'1','bsnl':'3','airtel':'28','vodafone':'22','docomo':'17','reliance':'13','idea':'8','uninor':'19','videocon':'5'}
+		HLR_to_op={'aircel':'1','bsnl':'3','cellone':'3','airtel':'28','vodafone':'22','docomo':'17','reliance':'13','idea':'8','uninor':'19','videocon':'5'}
 		op_code_map={'1':'AL','3':'BS','28':'AT','8':'IDX','10':'MS','12':'RL','13':'RG','17':'TD','19':'UN','5':'VD','22':'VF'}
 		for mccmnc in dict_HLR_op_code:
 			lookup_query=dict_HLR_op_code[mccmnc]
