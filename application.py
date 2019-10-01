72839a9beffa915f98eb3016921804b8a85b7175
rulebreakerdude <rulebreakerdude@gmail.com>
2019-10-01 17:43:03 +0530

    jio bugfix

--- application.py
+++ application.py
@@ -338,7 +338,7 @@ def rechargeSwara(pn,a,cc,wa,id,z):
 	rech=requests.post("http://www.login.imwallet.in/API/APIService.aspx?userid=6264241440&pass=819954&mob=%s&opt=%s&amt=%s&agentid=%s&fmt=JSON" %(pn,cc,a,z))
 	print rech.text
 	if json.loads(rech.text)['MSG'].split(',')[0]=='Failed':
-		imwallet_to_jolo={'AR':'AT','B':'BS','ID':'IDX','RG':'RG','DG':'TD','UN':'UN','VF':'VF','JO':'JO'}
+		imwallet_to_jolo={'AR':'AT','B':'BS','ID':'IDX','RG':'RG','DG':'TD','UN':'UN','VF':'VF','JIO':'JO'}
 		op_code=imwallet_to_jolo[cc]
 		rech=requests.get("https://joloapi.com/api/v1/recharge.php?userid=devansh76&key=326208132556249&operator=%s&service=%s&amount=%s&orderid=%s" % (op_code,pn,a,z))
 		if eval(rech.text)["status"] != 'FAILED':
